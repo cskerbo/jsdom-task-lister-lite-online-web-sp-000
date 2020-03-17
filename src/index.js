ccc561d8bc8d2a882fa1a1c16e33e27dc2262f07
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-17 22:32:34 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementByTag('form')
-  form.addEventListener('submit', addItem)
+
   let listItem = document.getElementById('tasks');
 
   function addItem(event) {
+    form.addEventListener('submit', addItem)
     event.preventDefault();
     let inputItem = document.getElementById('new-task-description').value
     listItem.innerHTML = `<li>inputItem</li>`
