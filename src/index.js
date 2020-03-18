efd559a3f708422433d761936ea6af0a63a52c36
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-18 00:04:06 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ document.addEventListener("DOMContentLoaded", () => {
     newLi.innerText = newTask
     const deleteButton = document.createElement('button')
     deleteButton.innerText = 'Delete Me'
-    
+
     myList.appendChild(newLi)
     myList.appendChild(deleteButton)
-  )}
+  )};
 
 });
