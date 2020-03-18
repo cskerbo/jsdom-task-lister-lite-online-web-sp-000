fd04b55188bf662860f26d563a1b7efed6fb6c59
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-18 00:04:03 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
 
   const taskInput = document.querySelector("#new-task-description")
   const taskForm = document.querySelector("#create-task-form")
 
-  taskForm.addEventListener('submit', function(e)) {
+  taskForm.addEventListener('submit', function(e) {
     e.preventDefault()
     console.log('Submitted')
     const newTask = taskInput.value
@@ -12,9 +12,9 @@ document.addEventListener("DOMContentLoaded", function() {
     newLi.innerText = newTask
     const deleteButton = document.createElement('button')
     deleteButton.innerText = 'Delete Me'
-
+    
     myList.appendChild(newLi)
     myList.appendChild(deleteButton)
-  }
+  )}
 
 });
